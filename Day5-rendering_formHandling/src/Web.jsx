496e731fb7b84d7d46f37add9bd2b2dbbe505c72
MdAamerSk <mdaamersk786@gmail.com>
2026-07-15 13:51:27 +0530

    feat: implement form handling inputs with bruteforce approach

--- Day5-rendering_formHandling/src/Web.jsx
+++ Day5-rendering_formHandling/src/Web.jsx
@@ -2,18 +2,43 @@ import React from 'react'
 import Card from './components/Card'
 import Contact from './components/Contact'
 import About from './components/About'
+import { useState } from 'react'
 
 const Web = () => {
-    let arr = [1, 3, 5, 6, 4, 2, 3, 8, 9, 5, 7, 8, 2, 3, 4, 5, 6, 7, 8, 9];
-    return (
-        <div className=' bg-gray-200'>
-            <h1>Web Page</h1>
 
-            {
-                arr.map((item,index) => {
-                    return <Card key={index} />
-                })
-            }
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    return (
+        <div className=" flex flex-col gap-5">
+            
+            <input
+                className="border-2 border-black"
+                type="text"
+                placeholder="Enter your name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            />
+            <input
+                className="border-2 border-black"
+                type="email"
+                placeholder="Enter your email"
+                value={email}
+                onChange={(e) => { setEmail(e.target.value) }}
+            />
+            <input
+                className="border-2 border-black"
+                type="password"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => { setPassword(e.target.value) }}
+            />
+
+            <h1 className="text-2xl font-bold">Hello, {name}</h1>
+            <h1 className="text-2xl font-bold">Your email is: {email}</h1>
+            <h1 className="text-2xl font-bold">Your password is: {password}</h1>
+
         </div>
     )
 }
