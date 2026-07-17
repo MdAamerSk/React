774de3a42d339b4b992fc6d8a6a37e9a95155fde
MdAamerSk <mdaamersk786@gmail.com>
2026-07-17 23:58:01 +0530

    feat: enhance form validation with error messages for name, email, mobile, and image fields

--- Day8-RHF_validation/src/components/Form.jsx
+++ Day8-RHF_validation/src/components/Form.jsx
@@ -9,10 +9,7 @@ const Form = ({ setUsers, setToggle }) => {
     formState: { errors },
   } = useForm({
     mode: "onChange",
-    defaultValues: {
-      name: "",
-      email: "",
-    },
+   
   });
 
   let formSubmit = (data) => {
@@ -30,33 +27,59 @@ const Form = ({ setUsers, setToggle }) => {
         className="w-90 flex flex-col bg-black gap-3 p-4 rounded border-2 border-white "
       >
         <input
-          {...register("name")}
+          {...register("name", {
+            required: "Name is required",
+            pattern: {
+              value: /^\S.*$/,
+              message: "Blank spaces is not allowed",
+            },
+          })}
           className="p-2 rounded outline-0 border border-white"
           type="text"
           placeholder="Name"
         />
-       
+        {errors.name && <p className="text-red-500">{errors.name.message}</p>}
         <input
-          {...register("email")}
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+              message: "Please enter valid email",
+            },
+          })}
           className="p-2 rounded outline-0 border border-white"
           type="email"
           placeholder="Email"
         />
-      
+        {errors.email && <p className="text-red-500">{errors.email.message}</p>}
         <input
-          {...register("mobile")}
+          {...register("mobile", {
+            required: "Mobile is required",
+            minLength: {
+              value: 10,
+              message: "Minimum 10 digits are required",
+            },
+            maxLength: {
+              value: 10,
+              message: "Maximum 10 digits are required",
+            },
+          })}
           className="p-2 rounded outline-0 border border-white"
           type="number"
           placeholder="Mobile"
         />
-       
+        {errors.mobile && (
+          <p className="text-red-500">{errors.mobile.message}</p>
+        )}
         <input
-          {...register("image")}
+          {...register("image", {
+            required: "Image is required",
+          })}
           className="p-2 rounded outline-0 border border-white"
           type="url"
           placeholder="Image"
         />
-       
+        {errors.image && <p className="text-red-500">{errors.image.message}</p>}
         <button className="text-white bg-blue-700 p-2 rounded-xl cursor-pointer">
           Add user
         </button>
