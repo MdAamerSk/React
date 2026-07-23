4687a5ded6f73015f4f99eb7698b0185c836bd4a
MdAamerSk <mdaamersk786@gmail.com>
2026-07-23 22:19:56 +0530

    feat: ProtectedRoute component for admin access control and update AppRoutes to integrate it

--- Day14-Dynamic_routing/src/routes/AppRoutes.jsx
+++ Day14-Dynamic_routing/src/routes/AppRoutes.jsx
@@ -1,21 +1,29 @@
 import React from 'react'
-import { Route,Routes } from 'react-router'
+import { Route, Routes } from 'react-router'
 import Home from '../pages/Home'
 import Products from '../pages/Products'
 import About from '../pages/About'
 import ProductDetail from '../pages/ProductDetail'
+import ProtectedRoute from "./ProtectedRoute";
 
 const AppRoutes = () => {
-  return (
-    <div>
-      <Routes>
-              <Route path='/' element={<Home />}></Route>
-              <Route path='/about' element={<About />}></Route>
-              <Route path='/products' element={<Products />}></Route>
-              <Route path='/detail/:id' element={<ProductDetail />}></Route> // dynamic routing
-     </Routes>
-    </div>
-  )
+    return (
+        <div>
+            <Routes>
+                <Route path='/' element={<Home />}></Route>
+                <Route
+                    path="/about"
+                    element={
+                        <ProtectedRoute>
+                            <About />
+                        </ProtectedRoute>
+                    }
+                />
+                <Route path='/products' element={<Products />}></Route>
+                <Route path='/detail/:id' element={<ProductDetail />}></Route> // dynamic routing
+            </Routes>
+        </div>
+    )
 }
 
 export default AppRoutes
