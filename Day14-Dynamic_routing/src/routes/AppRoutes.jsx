853160c21f1de9ceff8324ff5a93088ea42d8515
MdAamerSk <mdaamersk786@gmail.com>
2026-07-23 22:08:00 +0530

    feat:declarative approach - add ProductDetail component and integrate dynamic routing

--- Day14-Dynamic_routing/src/routes/AppRoutes.jsx
+++ Day14-Dynamic_routing/src/routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import { Route,Routes } from 'react-router'
 import Home from '../pages/Home'
 import Products from '../pages/Products'
 import About from '../pages/About'
+import ProductDetail from '../pages/ProductDetail'
 
 const AppRoutes = () => {
   return (
@@ -11,6 +12,7 @@ const AppRoutes = () => {
               <Route path='/' element={<Home />}></Route>
               <Route path='/about' element={<About />}></Route>
               <Route path='/products' element={<Products />}></Route>
+              <Route path='/detail/:id' element={<ProductDetail />}></Route> // dynamic routing
      </Routes>
     </div>
   )
