90143fd092036a1ac3ad1dce3160fd8ede246319
MdAamerSk <mdaamersk786@gmail.com>
2026-07-24 23:58:51 +0530

    feat: add Navbar component to Home, About, and Services pages for consistent navigation

--- Day15-Data_routing/src/pages/Home.jsx
+++ Day15-Data_routing/src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from '../component/Navbar'
 
 const Home = () => {
   return (
     <div>
+        <Navbar/>
       <h1>hey its home</h1>
     </div>
   )
--- Day15-Data_routing/src/pages/Services.jsx
+++ Day15-Data_routing/src/pages/Services.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import Navbar from '../component/Navbar'
 
 const Services = () => {
-  return (
-    <div>
-<h1>      hey its services</h1>
-    </div>
-  )
+    return (
+        <div>
+            <Navbar/>
+            <h1>hey its services</h1>
+        </div>
+    )
 }
 
 export default Services
