c6c1a293605612ab05d118d2d7b901dc2dd5774c
MdAamerSk <mdaamersk786@gmail.com>
2026-08-04 23:58:35 +0530

    feat: integrate toast notifications in Navbar and useAuth for user feedback

--- Day16-Project_interceptors/src/components/Navbar.jsx
+++ Day16-Project_interceptors/src/components/Navbar.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React,{ useContext } from 'react'
 import { Navigate, NavLink, useLocation } from "react-router";
+import { Auth } from "../context/AuthContext";
+import { toast } from "react-toastify";
 
 const Navbar = () => {
+
+  const { setLoggedInUser } = useContext(Auth);
+
   return (
    <div className="border-r border-gray-500 p-3 flex flex-col justify-between">
       <div className="flex flex-col  gap-10">
--- Day16-Project_interceptors/src/hooks/useAuth.jsx
+++ Day16-Project_interceptors/src/hooks/useAuth.jsx
@@ -1,4 +1,4 @@
-
+import { toast } from "react-toastify";
 import { useContext } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router";
