3fe5a1c78f34c76c5f99550f939a6b30cb5682fd
MdAamerSk <mdaamersk786@gmail.com>
2026-08-05 21:21:38 +0530

    feat: add useMemo for heavy calculation in App component and update rendering logic

--- Day17-Memoization/src/App.jsx
+++ Day17-Memoization/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Home from './components/Home'
 import About from './components/About'
-import { useState,useCallback } from 'react'
+import { useState,useCallback,useMemo } from 'react'
 
 const App = () => {
 
@@ -10,9 +10,19 @@ const App = () => {
 
   console.log("app rendering...");
 
+  let calculation = useMemo(() => {
+    console.log("calculation running....");
+    let sum = 0;
+
+    for (let i = 0; i < 100000000; i++) {
+      sum += i;
+    }
+    return sum;
+  }, [users]);
+
    let greet = useCallback(() => {
     console.log("good evening...");
-  }, [users]);
+  }, []);
 
   return (
     <div>
@@ -25,6 +35,9 @@ const App = () => {
       <button onClick={() => setCount(count + 1)} className="bg-blue-500 text-white px-4 py-2 rounded">
         increment
       </button>
+
+      <h2>My calculation is {calculation}</h2>
+
       <h2>Name is {users.name}</h2>
       <button onClick={() => setUsers({ ...users, name: "MD AAMER SHAIKH" })}> {/*hamesha naye object ka reference jata hai */}
         Change name{" "}
