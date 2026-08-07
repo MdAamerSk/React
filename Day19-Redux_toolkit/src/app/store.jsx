3ed53007a6bf6888c2b28b7049c9f29c77468247
MdAamerSk <mdaamersk786@gmail.com>
2026-08-07 19:42:19 +0530

    feat: add cart slice and integrate with Redux store

--- Day19-Redux_toolkit/src/app/store.jsx
+++ Day19-Redux_toolkit/src/app/store.jsx
@@ -1,9 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit'
 import counterReducer from '../features/counterSlice'
-
+import cartReducer from '../features/cartSlice'
 
 export const store = configureStore({
     reducer: {
         counter : counterReducer, // The key 'counter' represents the name of the slice, and the value 'counterReducer' is the reducer function for that slice. This sets up the Redux store with the counter slice's reducer, allowing the application to manage the state of the counter.
+        cart : cartReducer, // The key 'cart' represents the name of the slice, and the value 'cartReducer' is the reducer function for that slice. This sets up the Redux store with the cart slice's reducer, allowing the application to manage the state of the cart.
     },
 })
