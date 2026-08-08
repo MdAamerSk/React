1d32fe12292607464d67355542620f61a1ae6ed9
MdAamerSk <mdaamersk786@gmail.com>
2026-08-08 19:38:50 +0530

    feat: implement user hydration process to maintain authentication state across page refreshes

--- Day20-Authentication_redux/src/routes/AppRoutes.jsx
+++ Day20-Authentication_redux/src/routes/AppRoutes.jsx
@@ -6,9 +6,31 @@ import MainLayout from '../layouts/MainLayout'
 import { createBrowserRouter } from 'react-router'
 import LoginPage from '../pages/LoginPage'
 import RegisterPage from '../pages/RegisterPage'
-
+import { useDispatch } from 'react-redux'
+import { addUser } from '../features/authSlice'
+import { useEffect } from 'react'
+import { toast } from 'react-toastify'
 
 const AppRoutes = () => {
+
+    let dispatch = useDispatch();
+
+  const hydrateUser = () => { // this is a hydration process that checks if there is a logged in user in localStorage and if so, it dispatches the addUser action to add the user to the redux store. This is done so that the user remains logged in even after a page refresh.
+    console.log("hydration processed...");
+    let loggedInUser = JSON.parse(localStorage.getItem("loggedInUser"));
+
+    if (!loggedInUser) {
+      toast.error("UnAuthorized user");
+      return;
+    }
+
+    dispatch(addUser(loggedInUser));
+  };
+
+  useEffect(() => {
+    hydrateUser();
+  }, []);
+
 
     const router = createBrowserRouter([
         {
