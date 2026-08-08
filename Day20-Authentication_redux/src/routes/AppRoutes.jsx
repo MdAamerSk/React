49957ae1bccaafcc577e3807dc8aba6a86cd6080
MdAamerSk <mdaamersk786@gmail.com>
2026-08-08 19:49:27 +0530

    feat: add protected route components for public and main access control

--- Day20-Authentication_redux/src/routes/AppRoutes.jsx
+++ Day20-Authentication_redux/src/routes/AppRoutes.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import { RouterProvider } from 'react-router'
-import AuthLayout from '../layouts/AuthLayout'
-import Homepage from '../pages/Homepage'
-import MainLayout from '../layouts/MainLayout'
-import { createBrowserRouter } from 'react-router'
-import LoginPage from '../pages/LoginPage'
-import RegisterPage from '../pages/RegisterPage'
-import { useDispatch } from 'react-redux'
-import { addUser } from '../features/authSlice'
-import { useEffect } from 'react'
-import { toast } from 'react-toastify'
+import React, { useEffect } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import AuthLayout from "../layouts/AuthLayout";
+import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
+import MainLayout from "../layouts/MainLayout";
+import HomePage from "../pages/HomePage";
+import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
+import { addUser } from "../features/authSlice";
+import PublicProtected from "./protected/PublicProtected";
+import MainProtected from "./protected/MainProtected";
 
 const AppRoutes = () => {
+  let dispatch = useDispatch();
 
-    let dispatch = useDispatch();
-
-  const hydrateUser = () => { // this is a hydration process that checks if there is a logged in user in localStorage and if so, it dispatches the addUser action to add the user to the redux store. This is done so that the user remains logged in even after a page refresh.
+  const hydrateUser = () => {  // this is a hydration process that checks if there is a logged in user in localStorage and if so, it dispatches the addUser action to add the user to the redux store. This is done so that the user remains logged in even after a page refresh.
     console.log("hydration processed...");
     let loggedInUser = JSON.parse(localStorage.getItem("loggedInUser"));
 
@@ -31,34 +30,46 @@ const AppRoutes = () => {
     hydrateUser();
   }, []);
 
-
-    const router = createBrowserRouter([
+  let router = createBrowserRouter([
+    {
+      path: "/",
+      element: <PublicProtected />,
+      children: [
         {
-            path: '/',
-            element: <AuthLayout />,
-            children: [
-                {
-                    path: '',
-                    element: <LoginPage />
-                },
-                {
-                    path: 'register',
-                    element: <RegisterPage />
-                }
-            ]
+          path: "",
+          element: <AuthLayout />,
+          children: [
+            {
+              path: "",
+              element: <LoginPage />,
+            },
+            {
+              path: "register",
+              element: <RegisterPage />,
+            },
+          ],
         },
+      ],
+    },
+    {
+      path: "/main",
+      element: <MainProtected />,
+      children: [
         {
-            path: '/main',
-            element: <MainLayout />,
-            children: [
-                {
-                    path: '',
-                    element: <Homepage />
-                }
-            ]
-        }
-    ])
-    return <RouterProvider router={router} />
-}
+          path: "",
+          element: <MainLayout />,
+          children: [
+            {
+              path: "",
+              element: <HomePage />,
+            },
+          ],
+        },
+      ],
+    },
+  ]);
 
-export default AppRoutes
+  return <RouterProvider router={router} />;
+};
+
+export default AppRoutes;
