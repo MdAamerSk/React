a65802975bb55f31cd607291ff7fdbfc82e1eb47
MdAamerSk <mdaamersk786@gmail.com>
2026-08-09 11:47:52 +0530

    feat: implement code spilliting but messed up the  DRY for api call the soln is tanstack query which will be in next commit

--- Day21-Project_Tanstack_query/src/pages/ShopPage.jsx
+++ Day21-Project_Tanstack_query/src/pages/ShopPage.jsx
@@ -3,28 +3,21 @@ import ProductCard from "../components/ProductCard";
 import ProductCardSkeleton from "../components/ProductCardSkeleton";
 //import { useProduct, useProductApi } from "../hooks/productHooks";
 //import Filters from "../components/Filters";
-//import { getProductsDataApi } from "../api/productApi";
+import { getProductsDataApi } from "../api/productApi";
 
 const ShopPage = () => {
  const [productsData, setProductsData] = useState([]);
  const [isLoading, setIsLoading] = useState(true);
 
-  let getProductsData = async () => {
-     try {
-         let res = await fetch("https://dummyjson.com/products");
-         let data = await res.json(); // Parse the response as JSON
-         console.log(data.products);
-         setProductsData(data.products); // Update state to trigger render
-     } catch (error) {
-         console.log("error in product data fetching", error);
-     } finally {
-         setIsLoading(false);
-     }
+   const getData = async () => {
+    let data = await getProductsDataApi();
+    setProductsData(data);
+    setIsLoading(false);
   }
 
-    useEffect(() => {
-        getProductsData();
-    }, []);
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <div className="min-h-screen bg-black p-8">
