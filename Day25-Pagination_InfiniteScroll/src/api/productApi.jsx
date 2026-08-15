cd9cc06352989277e610cd8ff2b6d06d1da7eef8
MdAamerSk <mdaamersk786@gmail.com>
2026-08-15 15:13:29 +0530

    feat: implement infinite scrolling for product listing with React Query integration

--- Day25-Pagination_InfiniteScroll/src/api/productApi.jsx
+++ Day25-Pagination_InfiniteScroll/src/api/productApi.jsx
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-export const getAllProducts = async (limit, page = 1) => {
+export const getAllProducts = async (limit, pageParams) => {
   try {
+    console.log("page params -> ", pageParams);
     let res = await axios.get(
-      `https://dummyjson.com/products?limit=${limit}&skip=${page * limit}`
+      `https://dummyjson.com/products?limit=${limit}&skip=${pageParams}`
     );
     return res.data;
   } catch (error) {
