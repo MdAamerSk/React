80552a6aad8b88c181435c81495121277bd7b846
MdAamerSk <mdaamersk786@gmail.com>
2026-08-15 09:14:54 +0530

    feat: integrate TanStack for product listing with pagination using React Query

--- Day25-Pagination_InfiniteScroll/src/api/productApi.jsx
+++ Day25-Pagination_InfiniteScroll/src/api/productApi.jsx
@@ -1,2 +1,12 @@
-import axios from "axios"
+import axios from "axios";
 
+export const getAllProducts = async (limit, page = 1) => {
+  try {
+    let res = await axios.get(
+      `https://dummyjson.com/products?limit=${limit}&skip=${page * limit}`
+    );
+    return res.data;
+  } catch (error) {
+    console.log("Error in api", error);
+  }
+};
--- Day25-Pagination_InfiniteScroll/src/main.jsx
+++ Day25-Pagination_InfiniteScroll/src/main.jsx
@@ -1,7 +1,15 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import TanStack from "./TanStack.jsx";
 
-createRoot(document.getElementById('root')).render(
-    <App />
-)
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Create a client
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
+  <QueryClientProvider client={queryClient}>
+    <TanStack />
+  </QueryClientProvider>
+);
